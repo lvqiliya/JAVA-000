53a2969e3f57930d0ed4d8357cfbd58dbbcc7b85
lvqiliya <lvqiliya@gmail.com>
2020-10-19 22:31:11 +0800

    [update]The task Week01-2 completed

---
+++ Week_01/src/com/qly/Hello.java
@@ -0,0 +1,9 @@
+package com.qly;
+
+public class Hello {
+
+  public void hello(){
+     System.out.println("Hello, classLoader!");
+  }
+
+}
--- Week_01/src/com/qly/HelloClassLoaderTest.java
+++ Week_01/src/com/qly/HelloClassLoaderTest.java
@@ -1,14 +1,55 @@
 package com.qly;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
- * 加载 hello.xclass
+ * 加载 hello.xlass 并调用 hello() 方法
  *
  * @author qly
  */
 public class HelloClassLoaderTest {
 
     static class HelloClassLoader extends ClassLoader {
+        private final String path;
 
+        public HelloClassLoader(String path) {
+            this.path = path;
+        }
+
+        private byte[] loadFile(String name) throws IOException {
+            FileInputStream fis = new FileInputStream(this.path + name + ".xlass");
+            int len = fis.available();
+            byte[] data = new byte[len];
+            fis.read(data);
+            fis.close();
+            return data;
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            try {
+                byte[] data = loadFile(name);
+                for (int i = 0; i < data.length; i++) {
+                    data[i] = (byte) ((byte) 255 - data[i]);
+                }
+                return defineClass(name, data, 0, data.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new ClassNotFoundException();
+            }
+        }
     }
 
+    public static void main(String[] args)
+            throws ClassNotFoundException, IllegalAccessException,
+            InstantiationException, NoSuchMethodException, InvocationTargetException {
+        HelloClassLoader hcl = new HelloClassLoader("Week_01/resources/");
+        Class clazz = hcl.loadClass("Hello");
+        Object obj = clazz.newInstance();
+        Method method = clazz.getMethod("hello");
+        method.invoke(obj);
+    }
 }
